cbb41fb292477e5c31eb3a2e28fa87bf2a6c173b
creviherol <santiagoodiniz@hotmail.com>
2025-04-14 23:10:13 -0300

    Inplementação de controle automatico concluida

--- Logica fuzzy subimarino/Form1.cs
+++ Logica fuzzy subimarino/Form1.cs
@@ -20,11 +20,12 @@ namespace Logica_fuzzy_subimarino
         double HeatSupply;
         double CalorTubine;
         double Temperatura;
-        double Estabilida;
         int Carga = 3115;
         int Powermax = 3500;
         double DR;
         double DT;
+        int ConvReator;
+        int ConvTurbina;
         SerialPort portaSerial = new SerialPort("COM11", 9600); 
         
         public Form1()
@@ -52,22 +53,33 @@ namespace Logica_fuzzy_subimarino
 
             HeatSupply = (FuelPotential/100) * 2 * RateFission;
             CalorTubine = (Tubine);
-            Estabilida = (HeatSupply/2) - CalorTubine;
             Temperatura = HeatSupply - CalorTubine;
             DR = (((Powermax * FuelPotential * RateFission) / 7500) - Carga);
             DT = (((Powermax * Tubine) / 100) - Carga);
             textBox3.Text = "Calor gerado:" + HeatSupply;
             textBox4.Text = "Calor Tubina:" + (CalorTubine);
-            textBox5.Text = "Estabilidade:" + Estabilida;
             textBox6.Text = "Diferença de carga turbina:" + DT;
             textBox7.Text = "Diferença de carga reator:" + DR;
             textBox10.Text ="Temperatura: " + Temperatura;
 
             if (portaSerial.BytesToRead > 0){
+                RateFission = hScrollBar1.Value;
+                RateFission = RateFission / 100;
+                textBox1.Text = "Taxa de fissão:" + RateFission;
+                Tubine = hScrollBar2.Value;
+                Tubine = Tubine / 100;
+                textBox2.Text = "Tubina:" + Tubine;
                 string dado = portaSerial.ReadLine();
                 string[] partes = dado.Split(';');
                 textBox8.Text = partes[0];
                 textBox9.Text = partes[1];
+                ConvReator = (int)(double.Parse(partes[0]));
+                ConvTurbina = (int)(double.Parse(partes[1]));
+                textBox5.Text = ConvReator.ToString();
+                hScrollBar1.Value += ConvReator;
+                hScrollBar2.Value += ConvTurbina;
+                string mensagem = DR + ";" + DT + ";" + Temperatura;
+                portaSerial.WriteLine(mensagem);
             }
 
         }
@@ -80,7 +92,7 @@ namespace Logica_fuzzy_subimarino
 
         private void button2_Click(object sender, EventArgs e)
         {
-            
+            timer1.Enabled = !timer1.Enabled;
         }
     }
 }
